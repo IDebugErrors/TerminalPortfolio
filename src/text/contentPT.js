64a97bd708828313434506895040a9b161c64f0c
pate1838 <pate1838@sheridanc.on.ca>
2021-08-22 07:30:01 -0400

    first commit

---
+++ src/text/contentPT.js
@@ -0,0 +1,76 @@
+
+
+var contentEN = {
+    "aboutme" : {
+      "name" : "About me",
+      "content" : 
+  <>
+  <p>
+  Hello 👋, I'm <a rel="noreferrer" target="_blank" href="https://github.com/IDebugErrors">@JayPatel</a>,<br/>
+  Self Driven-Self Motivated Student Constantly Experimenting with new and old-school technologies. Areas of work : Software Engineering \ Cloud Computing . I like to program, I like to research and implement software design techniques, learn how to structure projects, and how to write elegant code.
+  </p>
+  <p>
+  Moreover, I like to study programming languages, functional programming, clean code, and talk about AI, open-source, PC Hardware and tech in general. 
+  </p>
+  <p>
+  I have developed personal projects in which I work with web apps, APIs, bots, and databases. I used concepts of functional programming, object-oriented programming, REST API, and image processing.
+  My main focus is on four core areas of software engineering as of now :
+  </p>
+  <ul>
+  <li>Programming and software development in Java, C and C++</li>
+  <li>Web technologies and web application development</li>
+  <li>Database management and administration</li>
+  <li>Network administration and security</li>
+  </ul>
+  </>
+    },
+  
+  
+    "skills" : {
+      "name" : "Skills", 
+      "content" : 
+  <>
+  <p>
+  •	Computer languages: Java, C#, JavaScript, TypeScript, PHP, PL/SQL, SQL, Bash, XML, HTML, CSS, JSON
+  </p>
+  <p>
+  •	Familiar IDEs: Visual Studio, Oracle SQL Developer, Android Studio, Unity, Netbeans
+  </p>
+  <p>
+  •	Proficient in different coding languages, able to transfer skills between languages and adapt
+  </p>
+  <p>
+  •	Other skills: Angular, React, Bootstrap, ASP.NET, JSP, MySQL, SASS, Git, GitHub, UNIX, Linux, jQuery.
+  </p>
+  <p>
+  •	Good understanding of OOP Concepts , Data Structures and Algorithms . 
+  </p>
+  <p>
+   Facinated by Cloud Technologies I aquired training for <a rel="noreferrer" target="_blank" href="https://docdro.id/dcICF2v">Cloud Practitioner</a> Fundamentals From AWS-Amazon. (On a path to become a certified AWS Solutions Architect) Also as a hobbyist gamer , I have tried developing Unity games (Earned Game Development Foundations <a rel="noreferrer" target="_blank" href="https://academy.zenva.com/certificate/ee54eabc">Certificate</a> from Zenva Academy)
+  </p>
+  </>
+    },
+  
+  
+    "contact" : {
+      "name" : "Contact",
+      "content" : 
+  <>
+  <p>I'm available to work on private or open-source projects</p>
+  <p>Fork/Open pull requests on my github project repositories (Some projects are still private and under development) or Shoot me an e-mail </p>
+  <p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/IDebugErrors">Github</a></p>
+  <p> 📨 <a href="mailto:jaypatel@programmer.net">Jaypatel@programmer.net </a></p>
+  <p> I will add Projects and Blogs section soon or (May be) I will develop a totally new portfolio as soon as I get a good front-end design Idea. </p>
+  <p> PEACE </p>
+  </>
+    }
+  }
+  
+  
+  export default contentEN
+  //jaypatel (jaypatel@programmer.net)
+  //jaypatel (jaypatel@programmer.net)
+  //jaypatel (jaypatel@programmer.net)
+  //jaypatel (jaypatel@programmer.net)
+  //jaypatel (jaypatel@programmer.net)
+  //jaypatel (jaypatel@programmer.net)
